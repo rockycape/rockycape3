78eeaf2afaa8452d7f6d9f4d772eac58f2ff59c5
LegendaryMagma <39517330+LegendaryMagma@users.noreply.github.com>
2024-06-12 14:13:14 +1000

    update

---
+++ _posts/RemoveAllNonCharacters.py
@@ -0,0 +1,10 @@
+import re
+
+def remove_non_chars(text):
+    # The regex pattern [^a-zA-Z] matches any non-letter character.
+    pattern = re.compile(r'[^a-zA-Z]')
+    cleaned_text = re.sub(pattern, '', text)
+    return cleaned_text
+
+string = "Scam, Sleep And Scam Again. Inside The Scam Facility Behind Those Annoying Text Messages - Abc News"
+print(remove_non_chars(string))
