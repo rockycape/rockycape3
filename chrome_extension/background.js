a282f2f7cd3869d8c156b56a4539665293951916
LegendaryMagma <39517330+LegendaryMagma@users.noreply.github.com>
2024-07-25 19:24:15 +1000

    update

--- chrome_extension/background.js
+++
@@ -1,8 +0,0 @@
-chrome.webNavigation.onCompleted.addListener(function(details) {
-    chrome.tabs.get(details.tabId, function(tab) {
-      if (tab.url.includes("smh.com.au")) {
-        const newUrl = tab.url.replace("smh.com.au", "theage.com.au");
-        chrome.tabs.update(tab.id, {url: newUrl});
-      }
-    });
-  }, {url: [{hostContains: "smh.com.au"}]});
